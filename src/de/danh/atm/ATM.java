9c987d48dbd03a349a2e87d96e7216e3029965fd
Danh Nguyen <danh.nguyen@kisters.de>
2019-09-27 12:12:16 +0200

    Bug Fixes

--- src/de/danh/atm/ATM.java
+++ src/de/danh/atm/ATM.java
@@ -1,10 +1,10 @@
 package de.danh.atm;
 
-import de.danh.atm.exceptions.InvalidAmountException;
-import de.danh.atm.exceptions.InvalidPinException;
 import de.danh.atm.models.Account;
 import de.danh.atm.models.Bank;
 
+import java.io.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -12,104 +12,259 @@ public class ATM {
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001b[31m";
-
-    private static int atmBalance = 150000;
     private static Account currentAccount;
-    private static Scanner scanner = new Scanner(System.in);
 
-    public static void login() {
+    /*
+     * checks your Username
+     */
+    public void login(Scanner scLogin) {
+        boolean status = false;
+
+        do {
+            System.out.println("Please enter your account name: ");
+            String name = " ";
+            if (scLogin.hasNextLine()) {
+                name = scLogin.nextLine();
+            }
+            for (Account account : Bank.getBank().getAccounts()) {
+                if (name.equals(account.getName())) {
+                    currentAccount = account;
+                    System.out.println("Welcome: " + ANSI_CYAN + name + ANSI_RESET + "!");
+                    status = true;
+                    break;
+                }
+            }
+            if (currentAccount == null) {
+                System.out.println(ANSI_RED + "Account not found" + ANSI_RESET);
+            }
+        } while (!status);
+    }
+
+    /*
+     * shows the Bank options, which are available
+     */
+    public void showOptions(Scanner option) {
+        boolean status = false;
+        do {
+            String inputOption;
+
+            System.out.println("--------------------------------------------------------");
+            System.out.println("Select your option:");
+            System.out.println("[1] - View Balance");
+            System.out.println("[2] - Withdraw Funds");
+            System.out.println("[3] - Deposit Funds");
+            System.out.println("[4] - Exit");
+            if (option.hasNextLine()) {
+                inputOption = option.nextLine();
+
+                try {
+                    int x = Integer.parseInt(inputOption);
+                    switch (x) {
+                        case 1:
+                            accountBalance(option);
+                            status = true;
+                            break;
+                        case 2:
+                            withdrawCash(option);
+                            status = true;
+                            break;
+                        case 3:
+                            deposit(option);
+                            status = true;
+                            break;
+                        case 4:
+                            System.out.println("have a nice day! \n");
+                            currentAccount.resetAttempts();
+                            currentAccount = null;
+                            status = true;
+                            break;
+                        default:
+                            System.out.println("Wrong input");
+                            System.out.println("Please use the numbers from 1-4");
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("Wrong input");
+                    System.out.println("Please use the numbers from 1-4");
+                }
+            }
+        } while (!status);
+    }
+
+    /* requests a PIN
+    if the pin was correct --> shows accounts balance
+     */
+    public void accountBalance(Scanner scBalance) {
+        boolean status = false;
+        try {
+            do {
+                System.out.println("Please enter your pin: ");
+                int pin = 0;
+                try {
+                    String input = scBalance.nextLine();
+                    pin = Integer.valueOf(input);
+                } catch (NumberFormatException e) {
+                    if (currentAccount.attempts() == 0) {
+                        currentAccount.outOfAttempts();
+                        break;
+                    } else if (currentAccount.attempts() > 0) {
+                        currentAccount.wrongPIN();
+                        System.out.println("Your tries: " + currentAccount.attempts());
+                    }
+                    continue;
+                }
+                if (currentAccount.checkPIN(pin)) {
+                    currentAccount.resetAttempts();
+                    currentAccount.showAccountBalance(pin);
+                    currentAccount.resetAttempts();
+                    currentAccount.showBalance();
+                    showOptions(scBalance);
+                    status = true;
+                } else {
+                    if (currentAccount.attempts() == 0) {
+                        currentAccount.outOfAttempts();
+                        break;
+                    } else if (currentAccount.attempts() > 0) {
+                        currentAccount.wrongPIN();
+                        currentAccount.attempts();
+                        System.out.println("Your tries: " + currentAccount.attempts());
+                    }
+                }
+            } while (!status);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /*
+    requests a PIN
+    if the pin was correct --> withdraws cash from balance
+     */
+    public void withdrawCash(Scanner scCash) {
+        boolean status = false;
+        try {
+            do {
+                System.out.println("Please enter your pin: ");
+                int pin = 0;
+                try {
+                    String input = scCash.nextLine();
+                    pin = Integer.valueOf(input);
+                } catch (NumberFormatException e) {
+                    if (currentAccount.attempts() == 0) {
+                        currentAccount.outOfAttempts();
+                        currentAccount = null;
+                        break;
+                    } else if (currentAccount.attempts() > 0) {
+                        currentAccount.wrongPIN();
+                        System.out.println("Your tries: " + currentAccount.attempts());
+                    }
+                    continue;
+                }
+
+                if (currentAccount.checkPIN(pin)) {
+                    currentAccount.resetAttempts();
+                    System.out.println("Please enter the amount you want to withdraw: ");
+                    double amount;
+                    String input = scCash.nextLine();
+                    try {
+                        amount = Double.parseDouble(input);
+                        currentAccount.withdrawMoney(amount);
+                        currentAccount.newBalance();
+                        showOptions(scCash);
+
+                    } catch (NumberFormatException e) {
+                        currentAccount.invalidAmount();
+                        showOptions(scCash);
+                    }
+                    status = true;
+                } else {
+                    //TODO Verschachtelung
+                    if (currentAccount.attempts() == 0) {
+                        currentAccount.outOfAttempts();
+                        currentAccount = null;
+                        break;
+                    } else if (currentAccount.attempts() > 0) {
+                        currentAccount.wrongPIN();
+                        System.out.println("Your tries: " + currentAccount.attempts());
+                    }
+                }
+            } while (!status);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*
+     * requests a PIN
+     * if the pin was correct --> deposits cash to balance
+     */
+    public void deposit(Scanner scDeposit) {
+        boolean status = false;
+
+        try {
+            do {
+                System.out.println("Please enter your pin: ");
+                int pin = 0;
+                try {
+                    String input = scDeposit.nextLine();
+                    pin = Integer.valueOf(input);
+                } catch (InputMismatchException e) {
+                    scDeposit.nextLine();
+                    currentAccount.wrongPIN();
+                    continue;
+                } catch (NumberFormatException e) {
+                    currentAccount.wrongPIN();
+                    continue;
+                }
+                if (currentAccount.checkPIN(pin)) {
+                    currentAccount.resetAttempts();
+                    System.out.println("Please enter the amount you want to deposit: ");
+                    double amount;
+                    String input = scDeposit.nextLine();
+                    try {
+                        amount = Double.parseDouble(input);
+                        currentAccount.depositMoney(amount);
+                        currentAccount.newBalance();
+                        showOptions(scDeposit);
+
+                    } catch (NumberFormatException e) {
+                        currentAccount.invalidAmount();
+                        showOptions(scDeposit);
+                    }
+                    status = true;
+                } else {
+                    if (currentAccount.attempts() == 0) {
+                        currentAccount.outOfAttempts();
+                        break;
+                    } else if (currentAccount.attempts() > 0) {
+                        currentAccount.wrongPIN();
+                        System.out.println("Your tries: " + currentAccount.attempts());
+                    }
+                }
+            } while (!status);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
         System.out.println(ANSI_CYAN + "[Welcome to my ATM Project!]" + ANSI_RESET);
-        System.out.println("Please enter your account name:");
-        String name = scanner.nextLine();
-        for (Account account : Bank.getBank().getAccounts()) {
-            if (name.equals(account.getName())) {
-                currentAccount = account;
-                System.out.println("Welcome: " + ANSI_RED + name + ANSI_RESET + "!");
-                showOptions();
-                break;
-            }
+        BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\dnguyen\\Desktop\\ATM\\src\\de\\danh\\atm\\users.txt")));
+        String currentLine = null;
+        while ((currentLine = br.readLine()) != null) {
+            System.out.println(currentLine);
+            String[] data = currentLine.split(" ");
+            Bank.getBank().addAccount(new Account(Double.valueOf(data[2]), data[0], Integer.valueOf(data[1])));
         }
-    }
-
-    public static void accountBalance() {
-        System.out.println("Please enter your pin: ");
-        int pin = scanner.nextInt();
-        try {
-            currentAccount.showAccountBalance(pin);
-            currentAccount.showBalance();
-            showOptions();
-        } catch (InvalidPinException e) {
-            System.out.println("You entered a wrong pin");
+        br.close();
+        ATM atm = new ATM();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            atm.login(scanner);
+            atm.showOptions(scanner);
         }
-    }
-
-    public static void withdrawCash() {
-        System.out.println("Please enter your pin: ");
-        int pin = scanner.nextInt();
-        System.out.println("Please enter the amount you want to withdraw: ");
-        int amount = scanner.nextInt();
-        try {
-            currentAccount.withdrawMoney(pin, amount);
-            currentAccount.newBalance();
-            showOptions();
-        } catch (InvalidPinException e) {
-            System.out.println("You entered a wrong pin.");
-        } catch (InvalidAmountException e) {
-            System.out.println("You entered an invalid withdrawal amount");
-        }
-    }
-
-    public static void deposit() {
-        System.out.println("Please enter your pin: ");
-        int pin = scanner.nextInt();
-        System.out.println("Please enter the amount you want to deposit: ");
-        int amount = scanner.nextInt();
-        try {
-            currentAccount.depositMoney(pin, amount);
-            currentAccount.newBalance();
-            showOptions();
-        } catch (InvalidPinException e) {
-            System.out.println("You entered the wrong pin.");
-        } catch (InvalidAmountException e) {
-            System.out.println("You entered an invalid deposit amount");
-        }
-    }
-
-    public static void showOptions() {
-        int x;
-        Scanner option = new Scanner(System.in);
-
-        System.out.println("--------------------------------------------------------");
-        System.out.println("Select your option:");
-        System.out.println("[1] - View Balance");
-        System.out.println("[2] - Withdraw Funds");
-        System.out.println("[3] - Deposit Funds");
-        System.out.println("[4] - Exit");
-        x = option.nextInt();
-
-        try {
-            switch (x) {
-                case 1:
-                    accountBalance();
-                    break;
-                case 2:
-                    withdrawCash();
-                    break;
-                case 3:
-                    deposit();
-                    break;
-                case 4:
-                    System.out.println("have a nice day!");
-                    System.exit(0);
-
-            }
-        } catch (InvalidOptionException e) {
-            System.out.println("This option is not available.");
-        }
-    }
-
-    public static void main(String[] args) {
-        Bank.getBank().addAccount(new Account(500.0, "danh", 1337));
-        login();
     }
 }
--- src/de/danh/atm/models/Account.java
+++ src/de/danh/atm/models/Account.java
@@ -6,14 +6,12 @@ import de.danh.atm.exceptions.InvalidPinException;
 public class Account {
     private double balance;
     private String name;
+    private int tries = 3;
     private int pin;
 
     public static final String ANSI_YELLOW = "\u001B[33m";
-
+    public static final String ANSI_RED = "\u001b[31m";
     public static final String ANSI_RESET = "\u001B[0m";
-
-    public Account() {
-    }
 
     public Account(double balance, String name, int pin) {
         this.balance = balance;
@@ -21,6 +19,9 @@ public class Account {
         this.pin = pin;
     }
 
+    /*
+    Returns account Balance
+     */
     public double showAccountBalance(int pin) {
         if (pin != this.pin) {
             throw new InvalidPinException("The pin you entered was wrong");
@@ -28,30 +29,42 @@ public class Account {
             return this.balance;
     }
 
+    /*
+    shows actual Balance
+     */
     public void showBalance() {
         System.out.println(ANSI_YELLOW + "Your Balance is: " + balance + "€" + ANSI_RESET);
     }
 
+    /*
+    shows new Balance
+     */
     public void newBalance() {
         System.out.println(ANSI_YELLOW + "Your new Balance is: " + balance + "€" + ANSI_RESET);
     }
 
-    public double withdrawMoney(int pin, int amount) {
-        if (pin != this.pin) {
-            throw new InvalidPinException("The pin you entered was wrong");
+    /*
+    returns account balance after it substracts the input amount with the balance
+     */
+    public double withdrawMoney(double amount) {
+        if (balance >= amount && amount >= 0) {
+            balance = balance - Double.valueOf(amount);
         }
-        if (balance < amount) {
-            throw new InvalidAmountException("The amount you entered was too high");
+        if (balance < amount || amount < 0) {
+            System.out.println("You entered an invalid withdrawal amount \n" + " or The amount you entered was too high");
         }
-        balance = balance - Double.valueOf(amount);
         return balance;
     }
 
-    public double depositMoney(int pin, int amount) {
-        if (pin != this.pin) {
-            throw new InvalidPinException("The pin you entered was wrong");
-        } else if (amount > 0) {
+    /*
+    returns the account balance after adding the input amount to the balance
+     */
+    public double depositMoney(double amount) {
+        if (amount > 0) {
             this.balance += Double.valueOf(amount);
+        }
+        if (amount < 0) {
+            System.out.println("The amount you entered is too low!");
         }
         return this.balance;
     }
@@ -60,7 +73,62 @@ public class Account {
         return name;
     }
 
+    /*
+    checks if the pin input was correct -> else { returns false }
+     */
     public boolean checkPIN(int pin) {
-        return pin == this.pin;
+        String inputPIN;
+
+        inputPIN = String.valueOf(pin);
+        if (inputPIN.matches("^[0-9]*$")) {
+            return pin == this.pin;
+        } else {
+            return false;
+        }
+    }
+
+    /*
+    indicates that you entered the wrong PIN
+    -> takes one try out
+    if the tries are below zero, then it will reset the amounts of attempts
+     */
+    public void wrongPIN() {
+        System.out.println(ANSI_RED + "The PIN you entered was wrong!" + ANSI_RESET);
+        if (tries > 0) {
+            tries--;
+        } else {
+            resetAttempts();
+        }
+    }
+
+    /*
+    resets the amount of attempts that you have
+     */
+    public void resetAttempts() {
+        tries = 3;
+    }
+
+    /*
+    will show up when you entered an incorrect PIN too many times
+    -> "suspends your account" : will exit the programm
+     */
+    public void outOfAttempts() {
+        System.out.println(ANSI_RED + "You've entered an incorrect PIN too many times." + ANSI_RESET);
+        System.out.println(ANSI_RED + "Your Account has been suspended!" + ANSI_RESET);
+        System.exit(0);
+    }
+
+    /*
+    returns tries
+     */
+    public int attempts() {
+        return tries;
+    }
+
+    /*
+    indicates that you have entered and invalid amount
+     */
+    public void invalidAmount() {
+        System.out.println(ANSI_RED + "You've entered an invalid amount" + ANSI_RESET);
     }
 }
